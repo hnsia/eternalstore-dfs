06207e259c5b23364675818c147d23458700ea95
hnsia <44498178+hnsia@users.noreply.github.com>
2025-01-02 21:36:48 +0800

    feat: implement TCP peers with distinction between inbound and outbound connections

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hnsia/eternalstore-dfs/p2p"
+)
 
 func main() {
-	fmt.Println("test")
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
 }
