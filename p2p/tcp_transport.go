8fce3b48ec6e7628e76223767fd97e46981a07b0
hnsia <44498178+hnsia@users.noreply.github.com>
2025-01-24 21:08:38 +0800

    feat: implement bootstrapping the network

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -59,6 +59,18 @@ func (t *TCPTransport) Close() error {
 	return t.listener.Close()
 }
 
+// Dial implements the Transport interface
+func (t *TCPTransport) Dial(addr string) error {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	go t.handleConn(conn, true)
+
+	return nil
+}
+
 func (t *TCPTransport) ListenAndAccept() error {
 	var err error
 
@@ -87,18 +99,18 @@ func (t *TCPTransport) startAcceptLoop() {
 
 		fmt.Printf("new incoming connection %+v\n", conn)
 
-		go t.handleConn(conn)
+		go t.handleConn(conn, false)
 	}
 }
 
-func (t *TCPTransport) handleConn(conn net.Conn) {
+func (t *TCPTransport) handleConn(conn net.Conn, outbound bool) {
 	var err error
 	defer func() {
 		fmt.Printf("dropping peer connection: %s", err)
 		conn.Close()
 	}()
 
-	peer := NewTCPPeer(conn, true)
+	peer := NewTCPPeer(conn, outbound)
 
 	if err = t.HandshakeFunc(peer); err != nil {
 		return
--- p2p/transport.go
+++ p2p/transport.go
@@ -8,6 +8,7 @@ type Peer interface {
 // Transport is anything that handles the communication
 // between the nodes in the network. This can be of the form (TCP, UDP, websockets, ...)
 type Transport interface {
+	Dial(string) error
 	ListenAndAccept() error
 	Consume() <-chan RPC
 	Close() error
