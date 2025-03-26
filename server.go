5917bdfc0140e9f75608b209f0d60231d52da695
hnsia <44498178+hnsia@users.noreply.github.com>
2025-03-26 22:52:35 +0800

    refactor: read method

--- server.go
+++ server.go
@@ -262,6 +262,11 @@ func (s *FileServer) handleMessageGetFile(from string, msg MessageGetFile) error
 		return err
 	}
 
+	if rc, ok := r.(io.ReadCloser); ok {
+		fmt.Println("closing ReadCloser")
+		defer rc.Close()
+	}
+
 	peer, ok := s.peers[from]
 	if !ok {
 		return fmt.Errorf("peer %s not in map", from)
